398f36fe9bf0ec4588ec2b40104c2643de2e6172
Avinash603 <avinash12ranjan@gmail.com>
2026-08-26 23:41:20 +0530

    Time: 1731 ms (23.95%), Space: 0B (100%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -4,6 +4,6 @@ FROM Visits v
 LEFT JOIN Transactions T
 ON v.visit_id= T.visit_id
 WHERE T.transaction_id IS NULL
-GROUP BY customer_id;
+GROUP BY v.customer_id;
 #jab v function use hota h count , avg ,sum y sab ko aggrgate function bolte h
 #us function se pahle jo column ho use grop by m likhte h 
